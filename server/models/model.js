71d86bb069c75fd31ccdb0a965192635f9c440e0
Mike Sahbaz <rsmikesahbaz@hotmail.com>
2023-04-19 12:03:38 -0600

    Added schema for Bugs and relationships

--- server/models/model.js
+++ server/models/model.js
@@ -1,9 +1,19 @@
+const { sequelize } = require(".")
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     username: {
       type: DataTypes.STRING,
@@ -80,8 +90,41 @@ module.exports = (sequelize, DataTypes) => {
   })
 }
 
+module.exports = (sequelize, DataTypes) => {
+  const Bug = sequelize.define('bug', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    priority: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    taskId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tasks',
+        key: 'id',
+      }
+    }
+  })
+}
+
 User.hasMany(Project, { foreignKey: 'userId' });
 Project.belongsTo(User, { foreignKey: 'userId' });
 
 Project.hasMany(Task, { foreignKey: 'projectId' });
 Task.belongsTo(Project, { foreignKey: 'projectId' });
+
+Task.hasMany(Bug, { foreignKey: 'taskId' });
+Bug.belongsTo(Task, { foreignKey: 'taskId' });
